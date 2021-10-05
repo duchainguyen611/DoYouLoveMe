90a959bbd875190c75d4a0f59cb4cfdb4b292f80
duchainguyen611 <duchaiprovip01@gmail.com>
2021-10-05 19:25:22 +0700

    up

---
+++ js/app.js
@@ -0,0 +1,5 @@
+function setRandomPosition() {
+        document.getElementById("btn-no").style.top = Math.floor(Math.random() * 200) + "px";
+        document.getElementById("btn-no").style.left = Math.floor(Math.random() * 400) + "px";
+        // document.getElementById("btn-no").style.right = Math.floor(Math.random()*300) + "px";
+}
